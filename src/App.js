e86dba04c2c7a283d5ff91815fdf01b78fd797dc
Vitoria Akemi <vitoriaakemitanimura@gmail.com>
2024-11-25 15:03:01 -0300

    criando e estilizando nanbar e footer

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import {BrowserRouter as Router, Routes, Route, Link  } from 'react-router-dom'
-import style from './components/layout/Container.module.css'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './components/pages/Home'
 import Contact from './components/pages/Contact'
 import Company from './components/pages/Company'
 import NewProject from './components/pages/NewProject'
+import Projects from './components/pages/Progects'
 import Container from './components/layout/Cointainer'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <Router>
-      <ul>
-        <Link to="/">Home</Link>
-        <Link to="/company">Contato</Link>
-        <Link to="/contact">Empresa</Link>
-        <Link to="/new-project">Novo Projeto</Link>
-      </ul>
+      <Navbar/>
       <Container customClass="min-height">
         <Routes>
           <Route exact path='/' element={<Home/>} />
           <Route exact path='/company' element={<Contact/>} />
           <Route exact path='/contact' element={<Company/>} />
           <Route exact path='/new-project' element={<NewProject/>} />
+          <Route exact path='/projects' element={<Projects/>} />
         </Routes>
       </Container>
-        <p>Footer</p>
+        <Footer/>
     </Router>
   );
 }
