b72623e3b3ca3317c0e6e374d9e6bb62a0280d0f
Vitoria Akemi <vitoriaakemitanimura@gmail.com>
2024-11-25 11:54:19 -0300

    fazendo roteamento

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import {BrowserRouter as Router, Routes, Route, Link  } from 'react-router-dom'
+import Home from './components/pages/Home'
+import Contact from './components/pages/Contact'
+import Company from './components/pages/Company'
+import NewProject from './components/pages/NewProject'
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/company">Contato</Link>
+        <Link to="/contact">Empresa</Link>
+        <Link to="/new-project">Novo Projeto</Link>
+      </ul>
+      <Routes>
+        <Route exact path='/' element={<Home/>} />
+        <Route exact path='/company' element={<Contact/>} />
+        <Route exact path='/contact' element={<Company/>} />
+        <Route exact path='/new-project' element={<NewProject/>} />
+      </Routes>
+        <p>Footer</p>
+    </Router>
   );
 }
 
